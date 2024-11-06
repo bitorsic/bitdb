2570f893818ed696c1feb9d77da2d35a9893b2e6
bitorsic <bitorsic@gmail.com>
2024-11-06 15:46:07 +0530

    first tree splitting implemented

--- src/index.c
+++ src/index.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "../include/utils.h"
 
 #define ORDER 4
@@ -29,9 +30,6 @@ struct Node* initNode() {
 
 	node->keyCount = 0;
 	node->isLeaf = true;
-	for (int i = 0;i < MAX_KEYS; i++) {
-		node->keys[i] = -1;
-	}
 	for (int i = 0; i < ORDER; i++) {
 		node->children[i] = NULL;
 	}
@@ -39,12 +37,74 @@ struct Node* initNode() {
 	return node;
 }
 
-void initRoot() {
-	root = initNode();
+struct Node* fetchOrCreateParentNode(struct Node* node) {
+	// base case
+	if (node == root) {
+		struct Node* tempRoot = initNode();
+		tempRoot->children[0] = node;
+		tempRoot->isLeaf = false;
+		root = tempRoot;
+		return root;
+	}
+		
+	// TODO: write logic to find the parent node
+
+	return NULL;
 }
 
-void freeRoot() {
-	free(root);
+void insertKeyInNode(int key, struct Node* node) {
+	// base case - empty node
+	if (node->keyCount == 0) {
+		node->keys[0] = key;
+		node->keyCount++;
+		return;
+	}
+
+	// firstly, check if the key already exists
+	int* findResult;
+	
+	findResult = findIndexForKey(key, node->keys, node->keyCount);
+	if (findResult[1] == 1) {
+		printf("Cannot insert %d as it is already present\n", key);
+		return;
+	}
+
+	int index = findResult[0]; // key should be inserted here
+	free(findResult);
+	
+	if (node->keyCount < MAX_KEYS) {
+		insertKeyAtIndex(key, index, node->keys, node->keyCount);
+		node->keyCount++;
+		return;
+	}
+
+	// here, node->keyCount = MAX_KEYS
+	// split tree, since the node has become full
+	
+	// insert the key in a temp sorted array
+	int tempArray[ORDER];
+	memcpy(tempArray, node->keys, 3 * sizeof(int));
+	insertKeyAtIndex(key, index, tempArray, ORDER); // since we already know the index
+
+	// set the index to where to split from
+	index = ORDER / 2;
+
+	// insert the key in the parent
+	struct Node* parent = fetchOrCreateParentNode(node);
+	insertKeyInNode(tempArray[index], parent);
+
+	// modify the current node as the left child
+	node->keyCount = index; // since we wanna copy until (index - 1)
+	memcpy(node->keys, tempArray, index * sizeof(int));
+
+	// now for the right child
+	int rightKeyCount = MAX_KEYS - index;
+	struct Node* rightChild = initNode();
+	rightChild->keyCount = rightKeyCount;
+	memcpy(rightChild->keys, &tempArray[index+1], rightKeyCount * sizeof(int));
+
+	// add the rightChild to the parent
+	parent->children[1] = rightChild; // TODO: insert the child at the correct index
 }
 
 void printTree(struct Node* root, int depth) {
@@ -75,36 +135,24 @@ void printTree(struct Node* root, int depth) {
     }
 }
 
-void insertKeyInNode(int key, struct Node* node) {
-	if (node->keyCount == 0) {
-		node->keys[0] = key;
-	} else if (node->keyCount < MAX_KEYS) {
-		int* findResult;
-		
-		findResult = findIndexForKey(key, node->keys, node->keyCount);
-		if (findResult[1] == 1) {
-			printf("Cannot insert %d as it is already present\n", key);
-			return;
-		}
+// ------------------------------------------------------ //
 
-		int index = findResult[0];
+void initRoot() {
+	root = initNode();
+}
 
-		insertKeyAtIndex(key, index, node->keys, node->keyCount);
-
-		free(findResult);
-	} else {
-		// TODO: write logic to split tree
-	}
-
-	node->keyCount++;
+void freeRoot() {
+	free(root);
 }
 
 void insertKeyInTree(int key) {
+	// base case
 	if (root->isLeaf) {
 		insertKeyInNode(key, root);
-	} else {
-		// TODO: write logic to traverse until leaf node arrives
+		return;
 	}
+
+	// TODO: write logic to traverse until leaf node arrives
 }
 
 void printEntireTree() {
--- src/main.c
+++ src/main.c
@@ -5,12 +5,32 @@
 int main() {
 	initRoot();
 
-	insertKeyInTree(10);
-	insertKeyInTree(30);
-	insertKeyInTree(20);
+	for(;;) {
+		int input;
+		printf("1. insert\n2. print\n3. exit\n");
+		printf(">> ");
+		scanf("%d", &input);
 
-	printEntireTree();
+		switch (input) {
+		case 1:
+			int key;
+			printf("key: ");
+			scanf("%d", &key);
+			insertKeyInTree(key);
+			break;
 
-	freeRoot();
+		case 2:
+			printEntireTree();
+			break;
+
+		case 3:
+			freeRoot();
+			exit(1);
+			
+		default:
+			break;
+		}
+	}
+
 	return 0;
 }
